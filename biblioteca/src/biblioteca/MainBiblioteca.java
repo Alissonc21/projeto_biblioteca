a1d513fdeb25d76492429130f8237a56525623a8
Alisson Silva <alissommartins@gmail.com>
2026-05-24 16:20:45 -0300

    Add lista de livros arraylist

--- biblioteca/src/biblioteca/MainBiblioteca.java
+++ biblioteca/src/biblioteca/MainBiblioteca.java
@@ -15,35 +15,35 @@ public class MainBiblioteca {
         ArrayList<String> locatarios = new ArrayList<>();
         ArrayList<Integer> prazos = new ArrayList<>();
 
-//        titulos.add("Grande Sertão: Veredas");
-//        autores.add("João Guimarães Rosa");
-//        status.add("Disponível");
-//        locatarios.add("Ninguém");
-//        prazos.add(0);
-//
-//        titulos.add("Dom Casmurro");
-//        autores.add("Machado de Assis");
-//        status.add("Disponível");
-//        locatarios.add("Ninguém");
-//        prazos.add(0);
-//
-//        titulos.add("Memórias Póstumas de Brás Cubas");
-//        autores.add("Machado de Assis");
-//        status.add("Emprestado");
-//        locatarios.add("Ninguém");
-//        prazos.add(0);
-//
-//        titulos.add("O Cortiço");
-//        autores.add("Aluísio Azevedo");
-//        status.add("Disponível");
-//        locatarios.add("Ninguém");
-//        prazos.add(0);
-//
-//        titulos.add("Vidas Secas");
-//        autores.add("Graciliano Ramos");
-//        status.add("Disponível");
-//        locatarios.add("Ninguém");
-//        prazos.add(0);
+        titulos.add("Grande Sertão: Veredas");
+        autores.add("João Guimarães Rosa");
+        status.add("Disponível");
+        locatarios.add("Ninguém");
+        prazos.add(0);
+
+        titulos.add("Dom Casmurro");
+        autores.add("Machado de Assis");
+        status.add("Disponível");
+        locatarios.add("Ninguém");
+        prazos.add(0);
+
+        titulos.add("Memórias Póstumas de Brás Cubas");
+        autores.add("Machado de Assis");
+        status.add("Emprestado");
+        locatarios.add("Ninguém");
+        prazos.add(0);
+
+        titulos.add("O Cortiço");
+        autores.add("Aluísio Azevedo");
+        status.add("Disponível");
+        locatarios.add("Ninguém");
+        prazos.add(0);
+
+        titulos.add("Vidas Secas");
+        autores.add("Graciliano Ramos");
+        status.add("Disponível");
+        locatarios.add("Ninguém");
+        prazos.add(0);
 
         Scanner lerTeclado = new Scanner(System.in);
         String menu = "";
